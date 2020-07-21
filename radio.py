c3e2b17dd8913c3f66716038795ac03c4f18ecb8
seepiol <seepiol@protonmail.com>
2020-07-21 12:31:17 +0200

    Merged bbc_radio.py and rai_radio.py into radio.py. Updated RAI stations. Update README.md

--- radio.py
+++ radio.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import sys
@@ -9,23 +9,24 @@ import subprocess
 import argparse
 
 # URL list taken from Wikipedia
-STATIONS = [
-    ['Radio 1', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_one/format/pls.pls'],
-    ['Radio 1Xtra', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_1xtra/format/pls.pls'],
-    ['Radio 2', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_two/format/pls.pls'],
-    ['Radio 3', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_three/format/pls.pls'],
-    ['Radio 4', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_fourfm/format/pls.pls'],
-    ['Radio 4Xtra', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_four_extra/format/pls.pls'],
-    ['Radio 5 Live', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_five_live/format/pls.pls'],
-    #['Radio 5 Live Sports Extra', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_five_live_sports_extra/format/pls.pls'],
-    ['Radio 6', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_6music/format/pls.pls'],
-    ['Asian Network', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_asian_network/format/pls.pls'],
-    ['Radio Scotland', 'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/ak/bbc_radio_scotland_fm.mpd'],
-    [u'Radio nan Gàidheal', 'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/llnw/bbc_radio_nan_gaidheal.mpd'],
-    ['Radio Wales', 'https://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/llnws/bbc_radio_wales_fm.mpd'],
-    ['Radio Cymru', 'https://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/llnws/bbc_radio_cymru.mpd'],
-    ['Radio Ulster', 'https://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/llnws/bbc_radio_ulster.mpd'],
-    ['Radio Foyle', 'https://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/aks/bbc_radio_foyle.mpd'],
+BBC_STATIONS = [
+    ['BBC World Service - English News', 'http://www.bbc.co.uk/worldservice/meta/live/mp3/ennws.pls'],
+    ['BBC Radio 1', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_one/format/pls.pls'],
+    ['BBC Radio 1Xtra', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_1xtra/format/pls.pls'],
+    ['BBC Radio 2', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_two/format/pls.pls'],
+    ['BBC Radio 3', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_three/format/pls.pls'],
+    ['BBC Radio 4', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_fourfm/format/pls.pls'],
+    ['BBC Radio 4Xtra', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_four_extra/format/pls.pls'],
+    ['BBC Radio 5 Live', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_five_live/format/pls.pls'],
+    ['BBC Radio 5 Live Sports Extra', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_radio_five_live_sports_extra/format/pls.pls'],
+    ['BBC Radio 6', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_6music/format/pls.pls'],
+    ['BBC Asian Network', 'http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/http-icy-mp3-a/vpid/bbc_asian_network/format/pls.pls'],
+    ['BBC Radio Scotland', 'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/ak/bbc_radio_scotland_fm.mpd'],
+    [u'BBC Radio nan Gàidheal', 'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/llnw/bbc_radio_nan_gaidheal.mpd'],
+    ['BBC Radio Wales', 'https://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/llnws/bbc_radio_wales_fm.mpd'],
+    ['BBC Radio Cymru', 'https://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/llnws/bbc_radio_cymru.mpd'],
+    ['BBC Radio Ulster', 'https://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/llnws/bbc_radio_ulster.mpd'],
+    ['BBC Radio Foyle', 'https://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/aks/bbc_radio_foyle.mpd'],
     ['BBC Berkshire', 'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/llnw/bbc_radio_berkshire.mpd'],
     ['BBC Bristol', 'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/llnw/bbc_radio_bristol.mpd'],
     ['BBC Cambridgeshire', 'https://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/aks/bbc_radio_cambridge.mpd'],
@@ -67,18 +68,34 @@ STATIONS = [
     ['BBC WM', 'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/llnw/bbc_wm.mpd'],
     ['BBC York', 'https://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/aks/bbc_radio_york.mpd'],
     ['BBC World Service - Internet Schedule', 'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/llnw/bbc_world_service.m3u8'],
-    ['BBC World Service - English News', 'http://www.bbc.co.uk/worldservice/meta/live/mp3/ennws.pls'],
     ['BBC World Service - Africa', 'http://www.bbc.co.uk/worldservice/meta/live/mp3/enafw.pls'],
     ['BBC Arabic Radio', 'https://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/dash/nonuk/dash_low/llnws/bbc_arabic_radio.mpd'],
 ]
-WIN_TITLE = "BBC radio"
+
+# URL list taken from http://icestreaming.rai.it/status.xsl. Renamed following https://www.raiplayradio.it/
+RAI_STATIONS = [
+    ['RAI Radio 1', 'http://icestreaming.rai.it/1.mp3'],
+    ['RAI Radio 1 Sport', 'http://icestreaming.rai.it/13.mp3'],
+    ['RAI Radio 2', 'http://icestreaming.rai.it/2.mp3'],
+    ['RAI Radio 2 Indie', 'http://icestreaming.rai.it/15.mp3'],
+    ['RAI Radio 3', 'http://icestreaming.rai.it/3.mp3'],
+    ['RAI Radio 3 Classica', 'http://icestreaming.rai.it/5.mp3'],
+    ['RAI IsoRadio', 'http://icestreaming.rai.it/6.mp3'],
+    ['RAI GrParlamento', 'http://icestreaming.rai.it/7.mp3'],
+    ['RAI Radio Kids', 'http://icestreaming.rai.it/11.mp3'],
+    ['RAI Radio Live', 'http://icestreaming.rai.it/10.mp3'],
+    ['RAI Radio Techete\'', 'http://icestreaming.rai.it/9.mp3'],
+    ['RAI Radio Tutta Italia', 'http://icestreaming.rai.it/11.mp3']
+]
+
+WIN_TITLE = "BBC&RAI Radio"
 
 class Win(QMainWindow):
     def __init__(self, parent=None):
         super(Win, self).__init__(parent)
         self.player = None
         # args
-        parser = argparse.ArgumentParser(description='BBC radio player')
+        parser = argparse.ArgumentParser(description='BBC&RAI Radio Player - Player of the British and Italian public player broadcaster')
         parser.add_argument('-p', '--player', default='vlc')
         parser.add_argument('player_args', nargs='*')
         args = parser.parse_args()
@@ -86,7 +103,7 @@ class Win(QMainWindow):
         self.player_args = args.player_args
         # UI
         self.setWindowTitle(WIN_TITLE)
-        self.setMinimumSize(300, 600)
+        self.setMinimumSize(500, 600)
         self.scroll_area = QScrollArea()
         self.widget = QWidget()
         self.layout = QVBoxLayout()
@@ -94,7 +111,7 @@ class Win(QMainWindow):
         self.scroll_area.setWidgetResizable(True)
         self.scroll_area.setWidget(self.widget)
         self.setCentralWidget(self.scroll_area)
-        for name, url in STATIONS:
+        for name, url in BBC_STATIONS:
             button = QPushButton(name.replace('&', '&&'))
             button.args = {
                 'name': name,
@@ -102,6 +119,16 @@ class Win(QMainWindow):
             }
             button.clicked.connect(self.listen)
             self.layout.addWidget(button)
+        
+        for name, url in RAI_STATIONS:
+            button = QPushButton(name.replace('&', '&&'))
+            button.args = {
+                'name': name,
+                'url': url,
+            }
+            button.clicked.connect(self.listen)
+            self.layout.addWidget(button)
+        
         # timer
         self.timer = QTimer()
         self.timer.timeout.connect(self.check_player)
